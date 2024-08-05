d9ce2c1695c52c4caf383728bcda667bd76987de
Nguyen Quang Minh <nguyenquangminh570@gmaiil.com>
2024-08-05 09:07:02 +0700

    feat: Add new todo page UI
    Adds a new page for adding new todos with the following features:
    
    - Text fields for task title, date, time
    , and description.
    - Category selection with color-coded icons.
    - Save button in the app bar.
    - Back button for navigation.

--- lib/futures/todo_list/presentation/pages/add_new_todo_page.dart
+++ lib/futures/todo_list/presentation/pages/add_new_todo_page.dart
@@ -1,14 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_list/core/themes/app_palette.dart';
+import 'package:todo_list/futures/todo_list/domain/entities/category.dart';
+import 'package:todo_list/futures/todo_list/presentation/widgets/todo_textfield.dart';
 
-class AddNewTodoPage extends StatelessWidget {
-  static route() =>
-      MaterialPageRoute(builder: (context) => const AddNewTodoPage());
+class AddNewTodoPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => AddNewTodoPage());
 
   const AddNewTodoPage({super.key});
 
   @override
+  State<AddNewTodoPage> createState() => _AddNewTodoPageState();
+}
+
+class _AddNewTodoPageState extends State<AddNewTodoPage> {
+  final titleController = TextEditingController();
+  final dateController = TextEditingController();
+  final timeController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppPalette.backgroundColor,
       appBar: AppBar(
         title: const Text(
           'Add New Task',
@@ -17,6 +37,24 @@ class AddNewTodoPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Add new task
+            },
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: const Icon(
+                Icons.check,
+                color: Colors.indigo,
+              ),
+            ),
+          ),
+        ],
         backgroundColor: Colors.indigo,
         leading: IconButton(
           onPressed: () {
@@ -31,8 +69,82 @@ class AddNewTodoPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text('Add New Todo Page'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TodoTextField(
+                textController: titleController,
+                hintText: 'Task Title',
+              ),
+              const SizedBox(height: 16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    const Text(
+                      'Category',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    for (final category in Category.categories)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: GestureDetector(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: category.colorUnCompleted,
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(
+                                color: category.colorUnCompleted,
+                                width: 2,
+                              ),
+                            ),
+                            child: CircleAvatar(
+                              backgroundColor: category.colorCompleted,
+                              child: Icon(
+                                category.icon,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: TodoTextField(
+                      textController: dateController,
+                      hintText: 'Date',
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    flex: 1,
+                    child: TodoTextField(
+                      textController: timeController,
+                      hintText: 'Time',
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              TodoTextField(
+                textController: descriptionController,
+                hintText: 'Description',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/futures/todo_list/presentation/widgets/todo_textfield.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TodoTextField extends StatelessWidget {
+  final TextEditingController textController;
+  final String hintText;
+
+  const TodoTextField({
+    super.key,
+    required this.textController,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          hintText,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: textController,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Colors.grey,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: Colors.grey,
+                width: 0.5,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: Colors.indigo,
+                width: 1,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: Colors.red,
+                width: 1,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
